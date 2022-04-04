b64e620c579bad6d7b856f9bd95ce856f3257aba
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-04-04 15:13:39 +0930

    added variables

---
+++ Assets/Javascript/main.js
@@ -0,0 +1,12 @@
+var startButton = document.querySelector("#startButton");
+var timerEl = document.querySelector('#timer');
+
+var questionEl = document.querySelector('#question');
+var b1El = document.querySelector('#b1');
+var b2El = document.querySelector('#b2');
+var b3El = document.querySelector('#b3');
+var b4El = document.querySelector('#b4');
+
+var introEl = document.querySelector("#intro");
+var quizEl = document.querySelector("#quiz");
+
